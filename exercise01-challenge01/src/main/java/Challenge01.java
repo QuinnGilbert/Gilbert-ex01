1173057d0641bcccc577bf2cf259377e0c60d926
QuinnG <quinngilbert12and3@gmail.com>
2021-12-01 21:24:24 -0500

    added challenge01

---
+++ exercise01-challenge01/src/main/java/Challenge01.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Exercise 1
+ *  Copyright 2021 Quinn Gilbert
+ */
+import java.util.*;
+public class Challenge01 {
+    public static void main(String[] args) {
+        System.out.print("What is your name? ");
+        System.out.printf("Hello, %s, nice to meet you!\n",new Scanner(System.in).nextLine());
+    }
+}
